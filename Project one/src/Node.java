03bf375fabcebb09d2da8860aa7145d81e6577bb
lienfrog <31316085+lienfrog@users.noreply.github.com>
2017-09-10 16:07:09 -0400

    Update Node.java

--- Project one/src/Node.java
+++ Project one/src/Node.java
@@ -3,18 +3,14 @@ import java.util.ArrayList;
 public class Node {
 	char state;
 	ArrayList<Edge> connections;
-	Path path;
+	ArrayList<Node> children = new ArrayList<Node>();
 	Node parent;
-	int depth;
-	float gCost;
-	float hCost;
-	float fCost;
+	int hCost;
 	boolean expanded;
 	
 	public Node(char state){
 		this.state = state;
 		connections = new ArrayList<Edge>();
-		path = null;
 		parent = null;
 		expanded = false;
 	}
@@ -23,26 +19,45 @@ public class Node {
 		hCost = newHeuristic;
 	}
 	
-	/*public void addChild(Node child) {
-		children.add(child);
+	public void addEdge(Edge edge) {
+		connections.add(edge);
 		
 	}
 	
-	public ArrayList<Node> expand() {
+	public ArrayList<Path> expand(Path oldPath) {
 		if(! expanded) {
+			ArrayList<Path> expPath = new ArrayList<Path>();
+			getChildren();
 			for(int i = 0;i < children.size();i++) {
 				if(!children.get(i).expanded) {
-					children.get(i).getPath().addAll(path.getPathNode());
-					children.get(i).getPath().addOne(0 ,children.get(i));
+					Path child = new Path();
+					child.addAll(oldPath.getPathNode());
+					child.addOne(0 ,children.get(i));
+					child.addGCost(connections.get(i).getWeight());
+					expPath.add(child);
+				}
 			}
-		}
-		return children;
+			return expPath;
 		}
 		return null;
-	}*/
+	}
 
-	public Path getPath() {
-		// TODO Auto-generated method stub
-		return path;
+	private void getChildren() {
+		for(int i = 0; i < connections.size(); i++) {
+			children.add(i,connections.get(i).getNodeCon(state));
+			
+		}
+		
+	}
+
+
+	public int getHCost() {
+		return hCost;
+	}
+	public void addHCost(int hCost) {
+		this.hCost = hCost;
+	}
+	public String getState() {
+		return state;
 	}
 }
