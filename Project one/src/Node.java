4cd11816dbd28cabdc5df48d27ddd60775d6f5f3
lienfrog <31316085+lienfrog@users.noreply.github.com>
2017-09-10 14:51:13 -0400

    Update Node.java

--- Project one/src/Node.java
+++ Project one/src/Node.java
@@ -4,15 +4,30 @@ public class Node {
 	String state;
 	ArrayList<Node> children;
 	Path path = null;
+	Node parent;
+	int depth;
+	int gCost;
+	int hCost;
+	int fCost;
+	boolean expanded = false;
 	
 	public Node(String state){
 		this.state = state;
+		
+	}
+	
+	public void addChild(Node child) {
+		children.add(child);
+		
 	}
 	
 	public ArrayList<Node> expand() {
+		if(! expanded) {
 		for(int i = 1;i <= children.size();i++) {
-			children.get(i).path().addall() 
+			children.get(i).path().addall(path);
 		}
 		return children;
+		}
+		return null;
 	}
 }
