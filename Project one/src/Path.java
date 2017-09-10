8a3975b180aad76a0917460c1375cde561d446ba
lienfrog <31316085+lienfrog@users.noreply.github.com>
2017-09-10 16:08:04 -0400

    Update Path.java

--- Project one/src/Path.java
+++ Project one/src/Path.java
@@ -2,18 +2,31 @@ import java.util.ArrayList;
 
 public class Path {
 	ArrayList<Node> nodes = new ArrayList<Node>();
+	int gCost;
+	int fCost;
+	int depth;
 	
-
 	public void addAll(ArrayList<Node> path) {
 		// TODO Auto-generated method stub
 		nodes.addAll(path);
 	}
 	
-	public void addOne(Node node) {
-		nodes.add(node);
+	public void addOne(int index, Node node) {
+		nodes.add(index, node);
 	}
 	
 	public ArrayList<Node> getPathNode(){
 		return nodes;
+	}
+	public Node getLastNode(){
+		return nodes.get(0);
+	}
+	
+	public int getFCost() {
+		fCost = gCost + nodes.get(0).getHCost();
+		return fCost;
+	}
+	public void addGCost(int newG) {
+		gCost += newG;
 	}
 }
