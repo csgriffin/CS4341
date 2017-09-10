5f9945a27a1870a29f264d18bb8ac91a25af9d65
Griffin <csgriffin@wpi.edu>
2017-09-10 15:07:02 -0400

    Add Edge class

---
+++ Project one/src/Edge.java
@@ -0,0 +1,16 @@
+
+public class Edge {
+
+	public Node firstNode;
+	public Node secondNode;
+	
+	public int edgeWeight;
+	
+
+	public Edge(Node firstNode, Node secondNode, int edgeWeight) {
+		this.firstNode = firstNode;
+		this.secondNode = secondNode;
+		this.edgeWeight = edgeWeight;
+	}
+	
+}
